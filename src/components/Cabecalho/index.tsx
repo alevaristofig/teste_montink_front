fc23fa8930d242a45e82deb9dfdc2999aaef126e
Alexandre <alevaristofig@gmail.com>
2025-07-21 23:40:16 -0300

    Implementação da função logou

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,15 +1,34 @@
-import React, { ReactElement, FormEvent } from 'react';
+import { ReactElement, FormEvent } from 'react';
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios';
 
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 
 const Cabecalho = (): ReactElement => {
 
-    const logout = () => {}
+    const navigate = useNavigate();
+
+    const logout = () => {
+        axios.get(`http://localhost:8000/api/erp_gerenciamento/logout`)         
+          .then(() => {                                			 
+			 sessionStorage.removeItem('token');
+			 sessionStorage.removeItem('id');
+
+			 navigate('/login', {replace: true});
+          })
+          .catch((erro) => {
+              toast.error('Ocorreu um erro e a operação não foi realizada');                  
+          });
+    }
 
     return (
         <>
             <Navbar bg='primary' className='menu_superior'>
+                <div>
+                    <ToastContainer />
+                </div>
                 <Container>
                     <div className='col-7 text-end'><h3 className='text-white'>ERP Gerenciamento</h3></div>
                 </Container>
